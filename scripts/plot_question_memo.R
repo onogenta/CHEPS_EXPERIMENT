491a58d0fbbd264c59933c269882088f713f045c
onogenta <ono@hebbs.emp.tsukuba.ac.jp>
2021-09-02 15:38:58 +0900

    アンケートプロットの修正

--- scripts/plot_question_memo.R
+++ scripts/plot_question_memo.R
@@ -1,10 +1,13 @@
 data_question<-data%>%
+  
+  mutate(simultaneity=if_else(simultaneity==1,true="simul",false="non_simul"))%>%
+  mutate(simultaneity=factor(simultaneity))%>%
   tidyr::gather(key=Question,value=rating,Q1,Q2,Q3,Q4)%>%
   filter(trial_num%%10==0)
   
 
 p_question<-ggplot(data_question,aes(x=Question,y=rating))+
-  geom_jitter(aes(color=Question),width=0.1,height = 0,show.legend = FALSE,size=3)+
+  geom_jitter(aes(color=Question,shape=simultaneity),width=0.1,height = 0,show.legend = TRUE,size=3)+
   geom_hline(linetype="dashed",yintercept = 50,col="black")+
   scale_y_continuous(limits = c(0,100))+
   theme_classic()
