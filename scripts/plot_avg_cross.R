6e361fd3a9889a1e933412c3210c48e15f610c80
onogenta <ono@hebbs.emp.tsukuba.ac.jp>
2021-10-08 15:13:20 +0900

    4条件箱ひげ図の追加

---
+++ scripts/plot_avg_cross.R
@@ -0,0 +1,65 @@
+data_load_pulse("all",load_multi_file = TRUE)
+# 
+# data_avg<-data%>%
+#   group_by(ID,delay,stimu_pos)%>%
+#   summarise(pain_avg=mean(rating_pain,na.rm = TRUE),unp_avg=mean(rating_unp,na.rm = TRUE),simul_avg=mean(simultaneity,na.rm=TRUE),Q1_avg=mean(Q1,na.rm = TRUE))%>%
+#   ungroup()
+
+data_diff<-data%>%
+  group_by(ID)%>%
+  mutate(pain_diff=rating_pain-mean(rating_pain,na.rm=TRUE),unp_diff=rating_unp-mean(rating_unp,na.rm=TRUE),Q1_diff=Q1-mean(Q1,na.rm=TRUE))%>%
+  ungroup()
+
+data_diff<-data_diff%>%
+  mutate(stimu_pos=if_else(stimu_pos==1,true = "wrist",false = "fore_arm"),simultaneity=if_else(simultaneity==1,true="simul",
+                                                                                                false = "non_simul"))
+
+data_diff$stimu_pos<- factor(data_diff$stimu_pos,levels = c("wrist","fore_arm"))
+
+
+data_diff_avg<-data_diff%>%
+  group_by(stimu_pos,simultaneity,ID)%>%
+  summarise(pain_diff_avg=mean(pain_diff,na.rm=TRUE),unp_diff_avg=mean(unp_diff,na.rm=TRUE),Q1_diff_avg=mean(Q1_diff,na.rm=TRUE))%>%
+  ungroup()
+
+
+# p_con_1<-ggplot(data=data_diff_avg,aes(x=simultaneity,y=pain_diff_avg,fill=stimu_pos))
+# 
+# p_con_2<-p_con_1+
+#   labs(x="",y="pain_rating")+
+#   scale_y_continuous(expand = c(0,0),limits = c(-20,20))+
+#   theme_classic()
+# 
+# p_con_3<-p_con_2+
+#   geom_boxplot(width=0.5,outlier.colour = "red",outlier.size = 3,outlier.shape = 8,position = position_dodge(1))+
+#   geom_jitter(size=1,position = position_jitterdodge(jitter.width = 0.1,jitter.height = 0,dodge.width = 1,seed = NA))+
+#   stat_summary(fun=mean,geom="point",size=3,shape=4,col="white",position = position_dodge(width = 1))
+
+p_con_pain<-ggplot(data=data_diff_avg,aes(x=simultaneity,y=pain_diff_avg,fill=stimu_pos))+
+  labs(x="",y="pain_rating")+
+  scale_y_continuous(expand = c(0,0),limits = c(-20,20))+
+  theme_classic()+
+  geom_boxplot(width=0.5,outlier.colour = "red",outlier.size = 3,outlier.shape = 8,position = position_dodge(1))+
+  geom_jitter(size=1,position = position_jitterdodge(jitter.width = 0.1,jitter.height = 0,dodge.width = 1,seed = NA))+
+  stat_summary(fun=mean,geom="point",size=3,shape=4,col="white",position = position_dodge(width = 1))
+
+
+p_con_unp<-ggplot(data=data_diff_avg,aes(x=simultaneity,y=unp_diff_avg,fill=stimu_pos))+
+  labs(x="",y="pain_rating")+
+  scale_y_continuous(expand = c(0,0),limits = c(-20,20))+
+  theme_classic()+
+  geom_boxplot(width=0.5,outlier.colour = "red",outlier.size = 3,outlier.shape = 8,position = position_dodge(1))+
+  geom_jitter(size=1,position = position_jitterdodge(jitter.width = 0.1,jitter.height = 0,dodge.width = 1,seed = NA))+
+  stat_summary(fun=mean,geom="point",size=3,shape=4,col="white",position = position_dodge(width = 1))
+
+p_con_Q1<-ggplot(data=data_diff_avg,aes(x=simultaneity,y=Q1_diff_avg,fill=stimu_pos))+
+  labs(x="",y="pain_rating")+
+  scale_y_continuous(expand = c(0,0),limits = c(-50,50))+
+  theme_classic()+
+  geom_boxplot(width=0.5,outlier.colour = "red",outlier.size = 3,outlier.shape = 8,position = position_dodge(1))+
+  geom_jitter(size=1,position = position_jitterdodge(jitter.width = 0.1,jitter.height = 0,dodge.width = 1,seed = NA))+
+  stat_summary(fun=mean,geom="point",size=3,shape=4,col="white",position = position_dodge(width = 1))
+
+ggsave(file = "pain_con_avg.png", plot = p_con_pain, dpi = 100, width = 16, height = 22)
+ggsave(file = "unp_con_avg.png", plot = p_con_unp, dpi = 100, width = 16, height = 22)
+ggsave(file = "Q1_con_avg.png", plot = p_con_Q1, dpi = 100, width = 16, height = 22)
